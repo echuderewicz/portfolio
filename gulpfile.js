253439bf48f13f7c0371e0e51405193f3d18ccfd
eric chuderewicz <echuderewicz@gmail.com>
2018-04-03 23:21:17 -0400

    added npm packages through lecture 12 from git a webdev job from udemy

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp'),
+watch = require('gulp-watch');
+
+// postcss = require('gulp-postcss'),
+// autoprefixer = require('autoprefixer'),
+// cssvars = require('postcss-simple-vars'),
+// nested = require('postcss-nested');
+
+gulp.task('default', function(){
+  console.log('The default works!');
+});
+
+gulp.task('html', function(){
+  console.log('Imagine something useful being done here');
+});
+
+//
+// gulp.task('styles', function(){
+//   return gulp.src('./app/assets/styles/styles.css')
+//   .pipe(postcss([cssvars, nested, autoprefixer]))
+//   .pipe(gulp.dest('./app/temp/styles'));
+//
+// });
+//
+// gulp.task('watch', function(){
+//   watch('./app/index.html', function(){
+//     gulp.start('html');
+//   });
+//
+//   watch('./app/assets/styles/**/*.css', function(){
+//     gulp.start('styles');
+//   })
+//
+// });
